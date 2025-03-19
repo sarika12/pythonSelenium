7c849730b02488d6636c4b5ee98b139522209c29
sarika12 <sarika1989.08@gmail.com>
2025-03-19 12:59:40 +0530

    pattern4.py

--- pattern/pattern4.py
+++ pattern/pattern4.py
@@ -1,3 +1,11 @@
 # Pattern-4: To print square pattern with alphabet symbols
+# Output:
+# Enter No Of Rows:5
+# A A A A A
+# B B B B B
+# C C C C C
+# D D D D D
+# E E E E E
 n=int(input("enter the input : "))
 for i in range(n):
+    print((chr(i+65)+" ")*n)
